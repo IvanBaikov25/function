a533c79d038e517d671c8152f27b4fa777aa88ad
IvanBaikov25 <bajkovivan07@gmail.com>
2025-03-07 06:03:26 +0300

    Update function2.php

--- function2.php
+++ function2.php
@@ -1,25 +1,22 @@
 <?php
-function isPerfectNumber($num) {
-    if ($num < 1) {
-        return false;
-    }
-    $sum = 0;
-    for ($i = 1; $i <= $num / 2; $i++) {
-        if ($num % $i === 0) {
-            $sum += $i;
-        }
-    }
-    return $sum === $num;
-}
-function findPerfectNumbers($arr) {
+function findPerfectNumbers(array $numbers): array {
     $perfectNumbers = [];
-    foreach ($arr as $number) {
-        if (isPerfectNumber($number)) {
+    foreach ($numbers as $number) {
+        if ($number > 1 && isPerfect($number)) {
             $perfectNumbers[] = $number;
         }
     }
     return $perfectNumbers;
 }
-$inputArray = [6, 28, 12, 496, 1000];
+function isPerfect(int $num): bool {
+    $sumOfDivisors = 0;
+    for ($i = 1; $i <= $num / 2; $i++) {
+        if ($num % $i === 0) {
+            $sumOfDivisors += $i;
+        }
+    }
+    return $sumOfDivisors === $num;
+}
+$inputArray = [6, 28, 12, 496, 15];
 $perfectNumbers = findPerfectNumbers($inputArray);
 print_r($perfectNumbers);
